f12dc16badd192828ce9e487f35921c784ebadb7
slavidichkov <dichkof@gmail.com>
2015-10-12 10:59:35 +0300

    Wrong commit

--- src/com/clouway/task5/timeouthastablelast/Demo.java
+++ src/com/clouway/task5/timeouthastablelast/Demo.java
@@ -1,5 +1,7 @@
 package com.clouway.task5.timeouthastablelast;
 
+import java.util.Date;
+
 /**
  * @author Slavi Dichkov (slavidichkof@gmail.com)
  */
@@ -7,17 +9,17 @@ public class Demo {
     public static void main(String[] args) throws InterruptedException {
         TimeoutHashtable hashtable=new TimeoutHashtable(1000);
 
-        hashtable.put("slavi","slavi");
-        hashtable.put("gosho","gosho");
+        hashtable.put("slavi",new Date());
+        hashtable.put("gosho",new Date());
         Thread.sleep(300);
         hashtable.print();
         System.out.println();
-        hashtable.put("nikola", "nikola");
+        hashtable.put("nikola", new Date());
         System.out.println(hashtable.get("gosho"));
         System.out.println();
         Thread.sleep(300);
-        hashtable.put("gosho", "gosho1");
-        hashtable.put("ivan", "ivan");
+        hashtable.put("gosho", new Date());
+        hashtable.put("ivan", new Date());
         hashtable.print();
         hashtable.remove("nikola");
         System.out.println();
