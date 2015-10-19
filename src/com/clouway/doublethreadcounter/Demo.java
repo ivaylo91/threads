1ae1fe2a0c3ab0cf6cf0500d63e41aee835b79e6
kristiyanpetkov <kristiqn.l.petkov@gmail.com>
2015-10-19 12:01:55 +0300

    initialisation changed in a right order

--- src/com/clouway/doublethreadcounter/Demo.java
+++ src/com/clouway/doublethreadcounter/Demo.java
@@ -7,9 +7,10 @@ public class Demo {
     public static void main(String[] args) {
         DoubleThreadCounter firstDoubleThreadCounter = new DoubleThreadCounter("First Thread", 8);
         DoubleThreadCounter secondDoubleThreadCounter = new DoubleThreadCounter("Second Thread", 16);
-        firstDoubleThreadCounter.start();
-        secondDoubleThreadCounter.start();
         firstDoubleThreadCounter.setThread(secondDoubleThreadCounter);
         secondDoubleThreadCounter.setThread(firstDoubleThreadCounter);
+        firstDoubleThreadCounter.start();
+        secondDoubleThreadCounter.start();
+
     }
 }
